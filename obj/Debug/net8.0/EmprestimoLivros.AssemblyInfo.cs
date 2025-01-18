3e512530794faef68cb53e42459732dfa6b8967b
Vinicius-DEV1 <vinicius.cl.dev@gmail.com>
2025-01-18 13:57:02 -0300

    Integrate DataTables for enhanced table functionality

--- obj/Debug/net8.0/EmprestimoLivros.AssemblyInfo.cs
+++ obj/Debug/net8.0/EmprestimoLivros.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmprestimoLivros")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21dc9056c510ebe684ec6fb42c4cead577cc6587")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8ac148f3aa8ac670f4d1ec97b2c61ee8613384f")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmprestimoLivros")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmprestimoLivros")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
